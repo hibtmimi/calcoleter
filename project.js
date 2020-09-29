8a30bf8106a0093f87f6bf6dd07dc32e5e36a21e
hibtmimi <hibatamimi24@gmail.com>
2020-09-29 18:01:08 +0300

    done

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-
+// add number 
   function insert(num){
    var myString = document.form.textview.value;
    var lastChar = myString[myString.length - 1];
@@ -10,15 +10,19 @@
     }
    }
   }
+  // sum the numbers 
   function equal(){
    var sum = document.form.textview.value;
    if (sum) {
     document.form.textview.value = eval(sum);
    }
   }
+  // delete the numbers 
   function clean(){
    document.form.textview.value = '';
   }
+  
+  // delete last number 
   function back(){
    var exp = document.form.textview.value;
    document.form.textview.value = exp.substring(0,exp.length - 1);
